c882182d16269f445b44a3e1830839e751dcf79d
Mohamed7Hossam <mohamed216975@bue.edu.eg>
2023-09-21 18:40:41 +0300

    -

--- lib/textpage.dart
+++ lib/textpage.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 class TextPage extends StatelessWidget {
   final String textK;
+
   const TextPage({super.key, required this.textK});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Center(
         child: Text(
           textK,
